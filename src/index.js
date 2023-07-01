3d08b934418c9f501e3758f2745a7c78e40647b7
piotrlewoniec <117780551+piotrlewoniec@users.noreply.github.com>
2023-07-01 17:27:20 +0200

    upload hw

--- src/index.js
+++ src/index.js
@@ -8,3 +8,17 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <App />
   </React.StrictMode>
 );
+
+// Kryteria zaliczenia
+// Użyj tego szablonu projektu React jako punktu wyjścia dla swojej aplikacji.
+// https://github.com/goitacademy/react-homework-template#readme
+// Utworzone zostały repozytoria goit-react-hw-03-phonebook i goit-react-hw-03-image-finder.
+// Zadanie domowe zawiera odnośniki: do plików źródłowych i strony roboczej projektów na GitHub Pages.
+// W stanie komponentów przechowywany jest minimalny niezbędny zestaw danych, pozostałe są obliczane.
+// Po uruchomieniu kodu zadania na konsoli nie ma błędów i ostrzeżeń.
+// Dla każdego komponentu istnieje oddzielny folder z plikiem komponentu React i plikiem stylów.
+// Dla komponentów opisane są propTypes.
+// Wszystko, czego komponent żąda w postaci propsów, jest do niego przekazywane przy wywołaniu.
+// Nazwy komponentów są zrozumiałe, opisowe.
+// Kod JS jest czysty i zrozumiały, wykorzystany został Prettier.
+// Stylizacja wykonana CSS-modułami lub Styled Components.
